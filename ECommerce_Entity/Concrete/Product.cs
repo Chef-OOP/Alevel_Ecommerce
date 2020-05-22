551830e3c34cd197e6a2dd3c3f80342f4fa7a3e7
sonerevran <sonerevrann@gmail.com>
2020-05-22 17:09:58 +0300

    deletecore

---
+++ ECommerce_Entity/Concrete/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce_Entity.Concrete
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+    }
+}
